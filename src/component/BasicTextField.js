789df90fc51a16a1517693f178ee5f8c364f4bf5
Halle Countryman <hkcountryman@gmail.com>
2021-06-18 20:20:08 -0700

    CategorySelect component outline (needs styling?)

--- src/component/BasicTextField.js
+++ src/component/BasicTextField.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import { InputLabel, MenuItem } from "@material-ui/core";
+import Select from '@material-ui/core/Select';
+import Paper from "@material-ui/core/Paper";
 
 // STYLING
 const useStyles = makeStyles((theme) => ({
@@ -12,7 +15,33 @@ const useStyles = makeStyles((theme) => ({
     label: {},
   },
 }));
+////////////////////////////////////////////////////////////////////////////////
+function CategorySelect() {
+  // category is "chores", "hobbies", "school", "self-care", "social", or "work"
+  const [category, setCategory] = React.useState("");
 
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  }
+  return (
+    <Paper id="category-selector-component">
+      <InputLabel>Category</InputLabel>
+      <Select
+        variant="outlined"
+        id="category-selection"
+        value={category}
+        onChange={handleCategoryChange}
+      >
+        <MenuItem value={"chores"}>Chores</MenuItem>
+        <MenuItem value={"hobbies"}>Hobbies</MenuItem>
+        <MenuItem value={"school"}>School</MenuItem>
+        <MenuItem value={"self-care"}>Self-care</MenuItem>
+        <MenuItem value={"Social"}>Social</MenuItem>
+        <MenuItem value={"work"}>Work</MenuItem>
+      </Select>
+    </Paper>
+  )
+}
 // EXPORT
 export default function BasicTextFields() {
   const classes = useStyles();
@@ -27,6 +56,7 @@ export default function BasicTextFields() {
           style: { textAlign: "left", verticalAlign: "center" },
         }}
       />
+      <CategorySelect/>
     </form>
   );
 }
